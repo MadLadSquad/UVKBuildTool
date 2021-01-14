6d84a38c0834e56db8967d4a42360dd29f2c4606
Madman10K <contact@madman10k.online>
2021-01-14 21:10:08 +0200

    There was a segfault

--- src/main.cpp
+++ src/main.cpp
@@ -41,38 +41,41 @@ int main(int argc, char** argv) {
         UBT::GenLVL(argv[2]);
         return 0;
     }
-
-    bool bIsInProduction;
-    const char* name;
-    auto config = YAML::LoadFile("../../uvproj.yaml");
-
-    if (config["production"])
+    else if (argv[1] == UBT::toLower("--generate"))
     {
-        bIsInProduction = config["production"].as<bool>();
+        bool bIsInProduction;
+        const char* name;
+        auto config = YAML::LoadFile("../../uvproj.yaml");
+
+        if (config["production"])
+        {
+            bIsInProduction = config["production"].as<bool>();
+        }
+
+        if (config["name"])
+        {
+            name = config["name"].as<std::string>().c_str();
+        }
+
+        if (config["renderer-subsystem"])
+        {
+            std::string str = UBT::toLower(config["renderer-subsystem"].as<std::string>());
+
+            if (str == "legacy")
+            {
+                UBT::GenerateFiles(1, bIsInProduction, name);
+            }
+            else if (str == "modern")
+            {
+                UBT::GenerateFiles(2, bIsInProduction, name);
+            }
+            else
+            {
+                std::cout << "You are trying to use an invalid subsystem! You can only submit legacy or modern as a renderer subsystem! Please fix your uvproj.yaml file!" << std::endl;
+            }
+        }
+
+        return 0;
     }
 
-    if (config["name"])
-    {
-        name = config["name"].as<std::string>().c_str();
-    }
-
-    if (config["renderer-subsystem"])
-    {
-        std::string str = UBT::toLower(config["renderer-subsystem"].as<std::string>());
-
-        if (str == "legacy")
-        {
-            UBT::GenerateFiles(1, bIsInProduction, name);
-        }
-        else if (str == "modern")
-        {
-            UBT::GenerateFiles(2, bIsInProduction, name);
-        }
-        else
-        {
-            std::cout << "You are trying to use an invalid subsystem! You can only submit legacy or modern as a renderer subsystem! Please fix your uvproj.yaml file!" << std::endl;
-        }
-    }
-
-    return 0;
 }
