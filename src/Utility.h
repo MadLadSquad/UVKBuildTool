e6ced41ae2ea09c415194bc818c694247e0bb8ce
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2021-03-29 10:41:01 +0300

    Update Utility.h

--- src/Utility.h
+++ src/Utility.h
@@ -155,7 +155,7 @@ int main(int argc, char** argv)
     }
     StartupLevel st;
 
-    StartupLevel::open(")" << startupLevelName << "\")" << std::endl;
+    StartupLevel::open(")" << startupLevelName << "\");" << std::endl;
         mainStream << R"(
 	UVK::Renderer(&st, bUsesEditor);
 })" << std::endl;
