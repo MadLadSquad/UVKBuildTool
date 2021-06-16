d8b62bc7be2bdcf2a5c1337ac178e4325ad626c4
Madman10K <contact@madman10k.online>
2021-06-16 15:08:51 +0300

    CMake cleaning

--- src/ReleaseBuild.cpp
+++ src/ReleaseBuild.cpp
@@ -9,6 +9,7 @@ void UBT::relBuild(int jobs)
     system(static_cast<std::string>("cd " + path + "Exported/ && cmake .. -G \"Unix Makefiles\" && make -j " + std::to_string(jobs) + " && cd ../").c_str());
 
     system(static_cast<std::string>("cd " + path + "Exported/ && rm -rf CMake* Makefile *.cmake && cd ../").c_str());
+    system(static_cast<std::string>("cd " + path + R"(Exported/ && find . -name '*.cmake' -delete && find . -type d -name "CMakeFiles" -exec rm -rf {} \; && cd ../)").c_str());
 
     std::ofstream out2(path + "Generated/BuildDef.hpp");
     out2 << "// Generated file, DO NOT TOUCH!" << std::endl;
