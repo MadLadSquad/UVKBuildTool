a5935a71d8e94c0ae2e501be5582a0ba70dccbe0
Madman10K <contact@madman10k.online>
2021-06-16 15:31:34 +0300

    CMake cleaning

--- src/ReleaseBuild.cpp
+++ src/ReleaseBuild.cpp
@@ -9,7 +9,9 @@ void UBT::relBuild(int jobs)
     system(static_cast<std::string>("cd " + path + "Exported/ && cmake .. -G \"Unix Makefiles\" && make -j " + std::to_string(jobs) + " && cd ../").c_str());
 
     system(static_cast<std::string>("cd " + path + "Exported/ && rm -rf CMake* Makefile *.cmake && cd ../").c_str());
-    system(static_cast<std::string>("cd " + path + R"(Exported/ && find . -name '*.cmake' -delete && find . -type d -name "CMakeFiles" -exec rm -rf {} \; && find Engine/ -name "Makefile" -exec rm -rf {} \; && find Engine/ -name "*.h" -exec rm -rf {} \; && find Engine/ -name "*.pc" -exec rm -rf {} \; && find . -type d -empty -exec rmdir {} \; && find . -type d -empty -exec rmdir {} \; && cd ../)").c_str());
+    system(static_cast<std::string>("cd " + path + R"(Exported/ && find . -name '*.cmake' -delete && find . -type d -name "CMakeFiles" -exec rm -rf {} \;)").c_str());
+    system(static_cast<std::string>("cd " + path + R"(Exported/ && find Engine/ -name "Makefile" -exec rm -rf {} \; && find Engine/ -name "*.h" -exec rm -rf {} \;)").c_str());
+    system(static_cast<std::string>("cd " + path + R"(Exported/ && find Engine/ -name "*.pc" -exec rm -rf {} \; && find . -type d -empty -exec rmdir {} \; && find . -type d -empty -exec rmdir {} \; && cd ../)").c_str());
 
     std::ofstream out2(path + "Generated/BuildDef.hpp");
     out2 << "// Generated file, DO NOT TOUCH!" << std::endl;
