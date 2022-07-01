af9806793999be5866fc810b1fb8d60de1a30b36
Madman10K <stanv.contact176@gmail.com>
2022-07-01 15:17:22 +0300

    Fixed build errors and updated dependencies

---
+++ ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cpus=$(grep -c processor /proc/cpuinfo)
+
+mkdir build && (cd build || exit)
+cmake .. || exit
+MSBuild.exe UVKBuildTool.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
+
+echo -e "\x1b[32mSuccessfully built the UVKBuildTool for the UntitledVulkanGameEngine!\x1b[0m"
+
+cd .. || exit
+rm -rf build && mkdir build
+cd build || exit
+
+cmake .. -DUBT_COMPILING_FOR_ENGINE=OFF || exit
+MSBuild.exe UVKBuildTool.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
+
+echo -e "\x1b[32mSuccessfully built the UVKBuildTool for the UntitledImGuiFramework!\x1b[0m"
