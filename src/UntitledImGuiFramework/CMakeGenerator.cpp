8ffa1ea69448b4b127a928163d65d8b8810b40b1
Madman10K <stanv.contact176@gmail.com>
2022-07-01 17:47:02 +0300

    Fixed build errors and updated dependencies

--- src/UntitledImGuiFramework/CMakeGenerator.cpp
+++ src/UntitledImGuiFramework/CMakeGenerator.cpp
@@ -4,7 +4,7 @@
 void UBT::generateCmake(const char* name)
 {
     UTG::Input in;
-    auto result = in.init("../Templates/BuildFiles/CMakeLists.txt.tmpl", UTG::Input::INPUT_TYPE_FILE);
+    auto result = in.init("../Templates/UntitledImGuiFramework/BuildFiles/CMakeLists.txt.tmpl", UTG::Input::INPUT_TYPE_FILE);
     if (result != UTG::Input::ERROR_TYPE_NO_ERROR)
     {
         std::cout << "\x1b[31mError when opening the CMakeLists.txt.tmpl file! Error code: " << static_cast<int>(result) << "\x1b[0m" << std::endl;
