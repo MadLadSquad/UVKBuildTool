85113df759f0d856b16c85524eb57ec53a7a66d4
Madman10K <stanv.contact176@gmail.com>
2023-08-09 19:44:01 +0300

    Ready for 1.0

--- ci.sh
+++ ci.sh
@@ -9,10 +9,5 @@ MSBuild.exe UVKBuildTool.sln -property:Configuration=Release -property:Platform=
 echo -e "\x1b[32mSuccessfully built the UVKBuildTool for the UntitledImGuiFramework!\x1b[0m"
 
 cd .. || exit
-rm -rf build && mkdir build
-cd build || exit
-
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DUBT_COMPILING_FOR_WEB=ON || exit
-MSBuild.exe UVKBuildTool.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
-
-echo -e "\x1b[32mSuccessfully built the UVKBuildTool for a web project!\x1b[0m"
+rm -rf build || exit
+./setup-web || exit
