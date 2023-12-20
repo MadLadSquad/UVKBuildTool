f0c80847974da9efe1c36271b26365cf3c137738
Madman10K <stanv.contact176@gmail.com>
2023-12-21 00:00:58 +0200

    Fix usability issues with the rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -3,13 +3,23 @@ if [[ $1 == "" ]]; then
   echo "ERROR: The script should be called with a CLI argument that specifies the project path!"
   exit
 fi
+
+if [[ $2 == "--web" ]]; then
+  web="ON"
+elif [[ $2 == "--framework" ]]; then
+  web="OFF"
+else
+  echo "ERROR: The script requires with either the '--web' or '--framework' options, depending on your project type!"
+  exit
+fi
+
 cpus=$(grep -c processor /proc/cpuinfo) || cpus=$(sysctl -n hw.ncpu)
 
 cp "$1/UBTCustomFunctions" src/Web/ -r || exit
 
 mkdir build &> /dev/null
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE || exit
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DUBT_COMPILING_FOR_WEB=ON || exit
 MSBuild.exe UVKBuildTool.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
 
 cp Release/UVKBuildTool.exe . 2> /dev/null || echo -n " " || exit
