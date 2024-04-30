bb44ebac60519c7422d0284aacce9126d4c7227e
Madman10K <stanv.contact176@gmail.com>
2024-04-30 14:39:20 +0300

    Move to pmap

--- src/Uniform/UniformUtility.cpp
+++ src/Uniform/UniformUtility.cpp
@@ -1,5 +1,35 @@
 #include "UniformUtility.h"
 #include <Generator.hpp>
+#include <yaml-cpp/yaml.h>
+
+namespace YAML
+{
+    // utte_map
+    template <typename K, typename V>
+    struct convert<utte_map<K, V>> {
+        static Node encode(const utte_map<K, V>& rhs) {
+            Node node(NodeType::Map);
+            for (const auto& element : rhs)
+                node.force_insert(element.first, element.second);
+            return node;
+        }
+
+        static bool decode(const Node& node, utte_map<K, V>& rhs) {
+            if (!node.IsMap())
+                return false;
+
+            rhs.clear();
+            for (const auto& element : node)
+#if defined(__GNUC__) && __GNUC__ < 4
+                // workaround for GCC 3:
+                rhs[element.first.template as<K>()] = element.second.template as<V>();
+#else
+                rhs[element.first.as<K>()] = element.second.as<V>();
+#endif
+            return true;
+        }
+    };
+}
 
 const std::string& UBT::getPath()
 {
