e85bbf0b640524fb8ef88be9f0394c0225ea29ac
Madman10K <stanv.contact176@gmail.com>
2024-05-20 23:01:00 +0300

    CLI Parser 3.0

--- main.cpp
+++ main.cpp
@@ -34,90 +34,149 @@ void getConfig(YAML::Node& config, std::string& name)
 int main(int argc, char** argv)
 {
     UBT::setPath("../../");
-    //UCLI::Parser parser{};
-    //parser.setUnknownArgumentCallback([](const char* name, void* data) -> void {
-    //    std::cout << ERROR << "Unknown argument error: " << name << std::endl;
-    //    // TODO: Add help function here
-    //}, nullptr);
-    //parser.parse(argc, argv, )
+    UCLI::Parser parser{};
+    parser.setUnknownArgumentCallback([](const char* name, void* data) -> void {
+        std::cout << ERROR << "Unknown argument error: " << name << END_COLOUR << std::endl;
+        exit(UBT::showHelp(false));
+    }, nullptr);
+    parser.parse(argc, argv, {
+            UCLI::Parser::ArrayFlag{
+                .longType = "generate",
+                .shortType = "g",
+                .func = [](UCLI::Parser::ArrayFlag*, char** args, size_t size) -> void {
+                    YAML::Node config;
+                    std::string name;
 
-    if (argc < 2)
-    {
-        std::cout << ERROR << "Not enough arguments passed into the generator!" << END_COLOUR << std::endl;
-        return UBT::showHelp(false);
-    }
-    if (argv[1] == UBT::toLower("--help"))
-        return UBT::showHelp(false);
+                    if (size == 0 || args == nullptr || args[0] == nullptr)
+                    {
+                        std::cout << ERROR << "Invalid argument, generate requires a path to a UVKBuildTool project!" << END_COLOUR << std::endl;
+                        exit(UBT::showHelp(true));
+                    }
+                    UBT::setPath(args[0]);
+                    getConfig(config, name);
 
-    YAML::Node config;
-    std::string name;
-    if (argv[1] == UBT::toLower("--generate") && argc < 4)
-    {
-        UBT::setPath(argv[2]);
-        getConfig(config, name);
+                    std::string startupLevelName;
+                    std::ifstream i(UBT::getPath() + "Generated/ActorList.hpp");
+                    if (config["name"])
+                        name = config["name"].as<std::string>();
 
-        std::string startupLevelName;
-        std::ifstream i(UBT::getPath() + "Generated/ActorList.hpp");
-        if (config["name"])
-            name = config["name"].as<std::string>();
+                    UBT::generateMain(name.c_str());
+                    UBT::generateDef();
+                    std::filesystem::copy_file(std::filesystem::path("../Templates/UntitledImGuiFramework/Sources/Config.hpp.tmpl"),
+                                               std::filesystem::path(UBT::getPath())/"Generated/Config.hpp");
+                },
+            },
+            UCLI::Parser::ArrayFlag{
+                .longType = "install",
+                .shortType = "i",
+                .func = [](UCLI::Parser::ArrayFlag*, char** args, size_t size) -> void {
+                    YAML::Node config;
+                    std::string name;
 
-        UBT::generateMain(name.c_str());
-        UBT::generateDef();
-        std::filesystem::copy_file(std::filesystem::path("../Templates/UntitledImGuiFramework/Sources/Config.hpp.tmpl"),
-                                   std::filesystem::path(UBT::getPath())/"Generated/Config.hpp");
-        return 0;
-    }
-    else if (argv[1] == UBT::toLower("--install") && argc < 4)
-    {
-        UBT::setPath(argv[2]);
-        getConfig(config, name);
+                    if (size == 0 || args == nullptr || args[0] == nullptr)
+                    {
+                        std::cout << ERROR << "Invalid argument, install requires a path to a UVKBuildTool project!" << END_COLOUR << std::endl;
+                        exit(UBT::showHelp(true));
+                    }
+                    UBT::setPath(args[0]);
+                    getConfig(config, name);
 
-        std::string startupLevelName;
-        std::ifstream i(UBT::getPath() + "Generated/ActorList.hpp");
-        if (config["name"])
-            name = config["name"].as<std::string>();
+                    std::string startupLevelName;
+                    std::ifstream i(UBT::getPath() + "Generated/ActorList.hpp");
+                    if (config["name"])
+                        name = config["name"].as<std::string>();
 
-        UBT::generateCmake(name.c_str());
-        UBT::generateMain(name.c_str());
-        UBT::generateDef();
-        UBT::makeTemplate(static_cast<std::string>(name + std::string("UIInstance")), "Instance", name.c_str());
+                    UBT::generateCmake(name.c_str());
+                    UBT::generateMain(name.c_str());
+                    UBT::generateDef();
+                    UBT::makeTemplate(static_cast<std::string>(name + std::string("UIInstance")), "Instance", name.c_str());
 
-        std::filesystem::copy_file(std::filesystem::path("../Templates/UntitledImGuiFramework/Sources/Config.hpp.tmpl"),
-                                   std::filesystem::path(UBT::getPath())/"Generated/Config.hpp");
+                    std::filesystem::copy_file(std::filesystem::path("../Templates/UntitledImGuiFramework/Sources/Config.hpp.tmpl"),
+                                               std::filesystem::path(UBT::getPath())/"Generated/Config.hpp");
+                },
+            },
+            UCLI::Parser::ArrayFlag{
+                .longType = "inline",
+                .shortType = "",
+                .func = [](UCLI::Parser::ArrayFlag*, char** args, size_t size) -> void {
+                    YAML::Node config;
+                    std::string name;
 
-        return 0;
-    }
+                    if (size < 2 || args == nullptr || args[0] == nullptr || args[1] == nullptr)
+                    {
+                        std::cout << ERROR << "Invalid argument, inline requires a class name path to a UVKBuildTool project!" << END_COLOUR << std::endl;
+                        exit(UBT::showHelp(true));
+                    }
+                    UBT::setPath(args[1]);
+                    getConfig(config, name);
 
-    if (argc < 4)
-    {
-        std::cout << ERROR << "Error: Not enough arguments passed into the generator!" << END_COLOUR << std::endl;
-        return UBT::showHelp(true);
-    }
-    else if (argc == 4)
-    {
-        UBT::setPath(argv[3]);
-        getConfig(config, name);
-        if (argv[1] == UBT::toLower("--inline"))
-            UBT::makeTemplate(std::string(argv[2]), "InlineComponent", name.c_str());
-        else if (argv[1] == UBT::toLower("--window"))
-            UBT::makeTemplate(std::string(argv[2]), "WindowComponent", name.c_str());
-        else if (argv[1] == UBT::toLower("--title-bar"))
-            UBT::makeTemplate(std::string(argv[2]), "TitlebarComponent", name.c_str());
-        return 0;
-    }
+                    UBT::makeTemplate(std::string(args[0]), "InlineComponent", name.c_str());
+                },
+            },
+            UCLI::Parser::ArrayFlag{
+                .longType = "window",
+                .shortType = "",
+                .func = [](UCLI::Parser::ArrayFlag*, char** args, size_t size) -> void {
+                    YAML::Node config;
+                    std::string name;
 
-    if (argc < 5)
-    {
-        std::cout << ERROR << "Error: Not enough arguments passed into the generator!" << END_COLOUR << std::endl;
-        return UBT::showHelp(true);
-    }
-    else if (argc == 5)
-    {
-        UBT::setPath(argv[4]);
-        getConfig(config, name);
-        if (argv[1] == UBT::toLower("--build"))
-            UBT::relBuild(config["name"].as<std::string>(), config, argv[2], argv[3]);
-    }
+                    if (size < 2 || args == nullptr || args[0] == nullptr || args[1] == nullptr)
+                    {
+                        std::cout << ERROR << "Invalid argument, window requires a class name and path to a UVKBuildTool project!" << END_COLOUR << std::endl;
+                        exit(UBT::showHelp(true));
+                    }
+                    UBT::setPath(args[1]);
+                    getConfig(config, name);
+
+                    UBT::makeTemplate(std::string(args[0]), "WindowComponent", name.c_str());
+                },
+            },
+            UCLI::Parser::ArrayFlag{
+                .longType = "title-bar",
+                .shortType = "",
+                .func = [](UCLI::Parser::ArrayFlag*, char** args, size_t size) -> void {
+                    YAML::Node config;
+                    std::string name;
+
+                    if (size < 2 || args == nullptr || args[0] == nullptr || args[1] == nullptr)
+                    {
+                        std::cout << ERROR << "Invalid argument, title-bar requires a class name and path to a UVKBuildTool project!" << END_COLOUR << std::endl;
+                        exit(UBT::showHelp(true));
+                    }
+                    UBT::setPath(args[1]);
+                    getConfig(config, name);
+
+                    UBT::makeTemplate(std::string(args[0]), "TitlebarComponent", name.c_str());
+                },
+            },
+            UCLI::Parser::ArrayFlag{
+                .longType = "build",
+                .shortType = "b",
+                .func = [](UCLI::Parser::ArrayFlag*, char** args, size_t size) -> void {
+                    YAML::Node config;
+                    std::string name;
+
+                    if (size < 2 || args == nullptr || args[0] == nullptr || args[1] == nullptr || args[2] == nullptr)
+                    {
+                        std::cout << ERROR << "Invalid argument, build requires a staging path, installation path and a path to a UVKBuildTool project!" << END_COLOUR << std::endl;
+                        exit(UBT::showHelp(true));
+                    }
+                    UBT::setPath(args[2]);
+                    getConfig(config, name);
+
+                    UBT::relBuild(config["name"].as<std::string>(), config, args[0], args[1]);
+                },
+            },
+        }, {}, {
+            UCLI::Parser::BooleanFlagWithFunc{
+                .longType = "help",
+                .shortType = "h",
+                .func = [](UCLI::Parser::BooleanFlagWithFunc*) -> void {
+                    exit(UBT::showHelp(false));
+                },
+            },
+        }
+    );
     return 0;
 }
 #elif UBT_TARGET_WEB
